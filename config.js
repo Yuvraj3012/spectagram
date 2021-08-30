728f73f64280244d2a6505c7f1c6c267108eeccd
Yuvraj3012 <71127295+Yuvraj3012@users.noreply.github.com>
2021-08-30 15:01:41 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+
+export  var  firebaseConfig = {
+  apiKey: "AIzaSyAKFIwBIGgMBMh2c6io0GAkqE8-Ft2ri9w",
+  authDomain: "instagram-b8004.firebaseapp.com",
+  databaseURL: "https://instagram-b8004-default-rtdb.firebaseio.com",
+  projectId: "instagram-b8004",
+  storageBucket: "instagram-b8004.appspot.com",
+  messagingSenderId: "747663622108",
+  appId: "1:747663622108:web:a1962968f7c336c32c5919"
+};
+
